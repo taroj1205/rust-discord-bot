ec46151d98cc4c70980e187a45a4d86d62d27afa
taroj1205 <taroj1205@gmail.com>
2024-11-21 16:25:52 +1300

    feat: initial commit

---
+++ src/main.rs
@@ -0,0 +1,94 @@
+#![allow(deprecated)]
+mod commands;
+
+use std::collections::HashSet;
+use std::sync::Arc;
+
+use serenity::async_trait;
+use serenity::framework::standard::macros::group;
+use serenity::framework::standard::Configuration;
+use serenity::framework::StandardFramework;
+use serenity::gateway::ShardManager;
+use serenity::http::Http;
+use serenity::model::event::ResumedEvent;
+use serenity::model::gateway::Ready;
+use serenity::prelude::*;
+use tracing::{error, info};
+use shuttle_secrets::SecretStore;
+use anyhow::anyhow;
+
+use crate::commands::math::*;
+use crate::commands::meta::*;
+use crate::commands::owner::*;
+
+pub struct ShardManagerContainer;
+
+impl TypeMapKey for ShardManagerContainer {
+    type Value = Arc<ShardManager>;
+}
+
+struct Handler;
+
+#[async_trait]
+impl EventHandler for Handler {
+    async fn ready(&self, _: Context, ready: Ready) {
+        info!("Connected as {}", ready.user.name);
+    }
+
+    async fn resume(&self, _: Context, _: ResumedEvent) {
+        info!("Resumed");
+    }
+}
+
+#[group]
+#[commands(multiply, ping, quit)]
+struct General;
+
+#[shuttle_runtime::main]
+async fn serenity(#[shuttle_secrets::Secrets] secret_store: SecretStore) -> shuttle_serenity::ShuttleSerenity {
+    // Get the discord token set in `Secrets.toml`
+    let token = secret_store
+        .get("DISCORD_TOKEN")
+        .ok_or_else(|| anyhow!("'DISCORD_TOKEN' was not found"))?;
+
+    let http = Http::new(&token);
+
+    // We will fetch your bot's owners and id
+    let (owners, bot_id) = match http.get_current_application_info().await {
+        Ok(info) => {
+            let mut owners = HashSet::new();
+            if let Some(team) = info.team {
+                owners.insert(team.owner_user_id);
+            } else {
+                owners.insert(info.owner.id);
+            }
+            match http.get_current_user().await {
+                Ok(bot_id) => (owners, bot_id.id),
+                Err(why) => panic!("Could not access the bot id: {:?}", why),
+            }
+        }
+        Err(why) => panic!("Could not access application info: {:?}", why),
+    };
+
+    let framework = StandardFramework::new()
+        .configure(|c| {
+            c.with_whitespace(true)
+                .on_mention(Some(bot_id))
+                .prefix("~")
+                .delimiters(vec![", ", ","])
+                .owners(owners)
+        })
+        .group(&GENERAL_GROUP);
+
+    let intents = GatewayIntents::GUILD_MESSAGES
+        | GatewayIntents::DIRECT_MESSAGES
+        | GatewayIntents::MESSAGE_CONTENT;
+
+    let client = Client::builder(&token, intents)
+        .event_handler(Handler)
+        .framework(framework)
+        .await
+        .expect("Err creating client");
+
+    Ok(client.into())
+}
