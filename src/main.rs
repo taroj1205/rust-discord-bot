13c199e4fa3a3e6c514ec9c845cee305823b85f5
taroj1205 <taroj1205@gmail.com>
2024-11-21 17:34:04 +1300

    fix(types): temorarily remove stuff

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,13 @@
 #![allow(deprecated)]
 mod commands;
 
-use std::collections::HashSet;
+// use std::collections::HashSet;
 use std::sync::Arc;
 
+use serenity::all::{ShardManager, StandardFramework};
 use serenity::async_trait;
 use serenity::framework::standard::macros::group;
-use serenity::framework::StandardFramework;
-use serenity::client::bridge::gateway::ShardManager;
-use serenity::http::Http;
+// use serenity::http::Http;
 use serenity::model::event::ResumedEvent;
 use serenity::model::gateway::Ready;
 use serenity::prelude::*;
@@ -48,34 +47,34 @@ async fn serenity(#[shuttle_runtime::Secrets] secret_store: SecretStore) -> shut
         .get("DISCORD_TOKEN")
         .ok_or_else(|| anyhow!("'DISCORD_TOKEN' was not found"))?;
 
-    let http = Http::new(&token);
+    // let http = Http::new(&token);
 
-    // We will fetch your bot's owners and id
-    let (owners, bot_id) = match http.get_current_application_info().await {
-        Ok(info) => {
-            let mut owners = HashSet::new();
-            if let Some(team) = info.team {
-                owners.insert(team.owner_user_id);
-            } else {
-                owners.insert(info.owner.id);
-            }
-            match http.get_current_user().await {
-                Ok(bot_id) => (owners, bot_id.id),
-                Err(why) => panic!("Could not access the bot id: {:?}", why),
-            }
-        }
-        Err(why) => panic!("Could not access application info: {:?}", why),
-    };
+    // // We will fetch your bot's owners and id
+    // let (owners, bot_id) = match http.get_current_application_info().await {
+    //     Ok(info) => {
+    //         let mut owners = HashSet::new();
+    //         if let Some(team) = info.team {
+    //             owners.insert(team.owner_user_id);
+    //         } else {
+    //             owners.insert(info.owner.id);
+    //         }
+    //         match http.get_current_user().await {
+    //             Ok(bot_id) => (owners, bot_id.id),
+    //             Err(why) => panic!("Could not access the bot id: {:?}", why),
+    //         }
+    //     }
+    //     Err(why) => panic!("Could not access application info: {:?}", why),
+    // };
 
-    let framework = StandardFramework::new()
-        .configure(|c| {
-            c.with_whitespace(true)
-                .on_mention(Some(bot_id))
-                .prefix("~")
-                .delimiters(vec![", ", ","])
-                .owners(owners)
-        })
-        .group(&GENERAL_GROUP);
+    // let framework = StandardFramework::new()
+    //     .configure(|c| {
+    //         c.with_whitespace(true)
+    //             .on_mention(Some(bot_id))
+    //             .prefix("~")
+    //             .delimiters(vec![", ", ","])
+    //             .owners(owners)
+    //     })
+    //     .group(&GENERAL_GROUP);
 
     let intents = GatewayIntents::GUILD_MESSAGES
         | GatewayIntents::DIRECT_MESSAGES
@@ -83,7 +82,7 @@ async fn serenity(#[shuttle_runtime::Secrets] secret_store: SecretStore) -> shut
 
     let client = Client::builder(&token, intents)
         .event_handler(Bot)
-        .framework(framework)
+        // .framework(framework)
         .await
         .expect("Err creating client");
 
